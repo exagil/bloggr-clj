783b3717977b84c723b08e79260b692f0eb32410
Chirag <me@chi6rag.net>
2017-06-26 17:56:09 +0530

    Making posts handler test unit

--- test/bloggr_clj/posts/handler_test.clj
+++ test/bloggr_clj/posts/handler_test.clj
@@ -7,10 +7,11 @@
             [bloggr-clj.posts.db :as posts-db]))
 
 (deftest test-that-the-posts-collection-is-empty-when-no-posts-exist
+  (with-redefs [posts-db/fetch-all-posts (fn [db-spec] '())]
   (is (= (bloggr-clj.posts.handler/all (ring.mock.request/request :get "/posts/"))
       {:status 200
       :headers {"Content-Type" "application/json"}
-      :body "[]"})))
+      :body "[]"}))))
 
 (deftest test-that-the-posts-collection-exists-when-posts-are-present
   (def posts [(post-record/->Post "First Title" "First Body")
