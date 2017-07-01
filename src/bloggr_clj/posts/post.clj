68f84f1bef1b87155ff690900cb770681b6171d3
Chirag <me@chi6rag.net>
2017-07-01 15:56:28 +0530

    Post has title blank error when title is blank

--- src/bloggr_clj/posts/post.clj
+++ src/bloggr_clj/posts/post.clj
@@ -3,4 +3,6 @@
 (defrecord Post [title body])
 
 (defn errors [record]
-  [])
+  (if (clojure.string/blank? (:title record))
+    ["Title is blank"]
+    []))
--- test/bloggr_clj/posts/post_test.clj
+++ test/bloggr_clj/posts/post_test.clj
@@ -6,3 +6,7 @@
 (deftest test-that-post-has-no-errors-when-details-are-valid
   (let [post (post-record/->Post "Valid Title" "Valid Message")]
   (is (= [] (post-record/errors post)))))
+
+(deftest test-that-post-has-a-title-blank-error-when-it-is-blank
+  (let [post (post-record/->Post "" "Valid Message")]
+  (is (= ["Title is blank"] (post-record/errors post)))))
